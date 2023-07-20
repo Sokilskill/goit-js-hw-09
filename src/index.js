2f29b5dc3f516c481445057c17e3354284821e73
Sokilskill <vprovorotov@gmail.com>
2023-07-20 20:44:12 +0300

    add files src

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './js/01-gallery';
-import './js/02-video.js';
-import './js/03-feedback';
--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,1 @@
-// const startButton = document.querySelector('data-start');
-// const stopButton = daocument.querySelector('data-stop');
-// console.log(startButton);
-// console.dir(stopButton);
+
